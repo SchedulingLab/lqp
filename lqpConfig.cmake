56004e0054ba040a156719572a5407f1549b5a23
Julien Bernard <julien.bernard@univ-fcomte.fr>
2022-04-17 20:57:53 +0200

    install Find*

--- lqpConfig.cmake
+++ lqpConfig.cmake
@@ -3,9 +3,11 @@ include (CMakeFindDependencyMacro)
 get_filename_component (LQP_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" DIRECTORY)
 include("${LQP_CMAKE_DIR}/lqpTargets.cmake")
 
+list(APPEND CMAKE_MODULE_PATH "${LQP_CMAKE_DIR}/cmake")
+
 if(NOT BUILD_SHARED_LIBS)
   find_dependency(GLPK)
-  # TODO: other dependecies?
+  # find_dependency(Gurobi)
 endif ()
 
 set(LQP_FOUND TRUE)
